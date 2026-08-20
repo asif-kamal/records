164325f8ef750fc65cb96c6d711919161adc3b95
asif-kamal <asif.kamal1489@gmail.com>
2026-08-20 16:04:35 -0500

    using contains to match regular expression

--- src/main/java/RegularExpressions.java
+++ src/main/java/RegularExpressions.java
@@ -4,5 +4,17 @@ public class RegularExpressions {
         String helloWorld2 = String.format("%s %s", "Hello", "World");
         System.out.println("with formatted method: " + helloWorld);
         System.out.println("with format method: " + helloWorld2);
+
+        String helloWorld3 = RegularExpressions.format("%s %s", "Hello", "World");
+        System.out.println("with custom format method: " + helloWorld3);
+    }
+
+    private static String format(String regexp, String... args) {
+
+        int index = 0;
+        while (regexp.contains("%s")) {
+            regexp = regexp.replaceFirst("%s", args[index++]);
+        }
+        return regexp;
     }
 }
