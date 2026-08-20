858364a309743366d166060102f965fbdfda8e0d
asif-kamal <asif.kamal1489@gmail.com>
2026-08-20 16:05:31 -0500

    using matches with dot and asterisk to match regular expression

--- src/main/java/RegularExpressions.java
+++ src/main/java/RegularExpressions.java
@@ -12,7 +12,7 @@ public class RegularExpressions {
     private static String format(String regexp, String... args) {
 
         int index = 0;
-        while (regexp.contains("%s")) {
+        while (regexp.matches(".*%s.*")) {
             regexp = regexp.replaceFirst("%s", args[index++]);
         }
         return regexp;
