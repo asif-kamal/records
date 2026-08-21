977b6e32d82a492f968557c5f8e155b5db3f5772
asif-kamal <asif.kamal1489@gmail.com>
2026-08-21 17:09:50 -0500

    [0-9] matches the empty string at the start, because  allows zero matches

--- src/main/java/RegularExpressions.java
+++ src/main/java/RegularExpressions.java
@@ -11,9 +11,9 @@ public class RegularExpressions {
         String testString = "Anyone can Learn abc's, 123's, and any regular expression";
         String replacement = "(-)";
         String[] patterns = {
-                "[a-z]",
-                "[0-9]",
-                "[A-Z]"
+                "[a-zA-Z]*",
+                "[0-9]*",
+                "[A-Z]*"
         };
 
         for (String pattern : patterns) {
